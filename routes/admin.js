51e3211f56e9ab745a8a26960dbdbd5cc8234f01
Nikola <nikislavchev1@abv.bg>
2022-04-22 23:58:29 +0300

    Admin - product management

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router()
+const Product = require('../models/product')
 
-router.get('/', (req, res) => {
+function authorize(req, res) {
     const reject = () => {
         res.setHeader('www-authenticate', 'Basic')
         res.sendStatus(401)
@@ -18,8 +19,51 @@ router.get('/', (req, res) => {
     if (!(username === 'nikola' && password === 'difpass6y7')) {
         return reject()
     }
+}
 
-    res.send('Top secret stuff here')
+// All Products Route
+router.get('/', async (req, res) => {
+    authorize(req, res)
+
+    let searchOptions = {}
+    if (req.query.name != null && req.query.name !== '') {
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+
+    try {
+        const products = await Product.find(searchOptions)
+        res.render('admin/index', { 
+            products: products, 
+            searchOptions: req.query 
+        })
+    } catch {
+        res.redirect('/')
+    }
+})
+
+// New Product Route
+router.get('/new', (req, res) => {
+    authorize(req, res)
+
+    res.render('admin/new-product', { product: new Product() })
+})
+
+// Create Product Route
+router.post('/', async (req, res) => {
+    authorize(req, res)
+
+    const product = new Product({
+        name: req.body.name
+    })
+    try {
+        const newProduct = await product.save()
+        res.redirect(`admin`)
+    } catch {
+        res.render('admin/new-product', {
+            product: product,
+            errorMessage: '___Грешка при добавянето на продукт___'
+        })
+    }
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
 const adminRouter = require('./routes/admin')
@@ -14,9 +15,10 @@ app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
