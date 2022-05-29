dfac3f589fb875411d7022bdde8eb4ed053fb2fd
Nikola <nikislavchev1@abv.bg>
2022-05-29 19:09:22 +0300

    frontend updates

--- routes/admin.js
+++ routes/admin.js
@@ -67,31 +67,45 @@ router.get('/', async (req, res) => {
     }
 
     let query = Product.find()
+    let emptyQuery = true
     if (req.query.title != null && req.query.title != '') {
         query = query.regex('title', new RegExp(algs.searchAlg(req.query.title), 'i'))
+        emptyQuery = false
     }
     if (req.query.description != null && req.query.description != '') {
         query = query.regex('description', new RegExp(algs.searchAlg(req.query.description), 'i'))
+        emptyQuery = false
     }
     if (req.query.minPricePerPiece != null && req.query.minPricePerPiece != '') {
         query = query.gte('pricePerPiece', req.query.minPricePerPiece)
+        emptyQuery = false
     }
     if (req.query.maxPricePerPiece != null && req.query.maxPricePerPiece != '') {
         query = query.lte('pricePerPiece', req.query.maxPricePerPiece)
+        emptyQuery = false
     }
     if (req.query.minPricePerUnit != null && req.query.minPricePerUnit != '') {
         query = query.gte('pricePerUnit', req.query.minPricePerUnit)
+        emptyQuery = false
     }
     if (req.query.maxPricePerUnit != null && req.query.maxPricePerUnit != '') {
         query = query.lte('pricePerUnit', req.query.maxPricePerUnit)
+        emptyQuery = false
     }
 
     try {
-        const products = await query.exec()
-        res.render('admin/index', { 
-            products: products, 
-            searchOptions: req.query 
-        })
+        if (emptyQuery) {
+            res.render('admin/index', { 
+                products: null, 
+                searchOptions: req.query 
+            })
+        } else {
+            const products = await query.exec()
+            res.render('admin/index', { 
+                products: products, 
+                searchOptions: req.query 
+            })
+        }
     } catch {
         res.redirect('/')
     }
