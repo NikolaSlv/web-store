193c879a7dc782dff5eedaedb526345638436c51
Nikola <nikislavchev1@abv.bg>
2022-06-01 11:11:53 +0300

    css optimization + noSQL injection prevention

--- server.js
+++ server.js
@@ -4,18 +4,18 @@ if (process.env.NODE_ENV !== 'production') {
 
 const express = require('express')
 const compression = require('compression')
-const favicon = require('serve-favicon');
 
 const app = express()
 
 app.use(compression({
     level: 6
 }))
-app.use(favicon(__dirname + '/public/images/favicon.ico'));
 
+const favicon = require('serve-favicon');
 const expressLayouts = require('express-ejs-layouts')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const filter = require('content-filter')
 
 const indexRouter = require('./routes/index')
 const adminRouter = require('./routes/admin')
@@ -24,10 +24,21 @@ const productsRouter = require('./routes/products')
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
+
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
 app.use(expressLayouts)
 app.use(methodOverride('_method'))
 app.use(express.static('public', { maxAge: '7d' }))
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
+
+const blackList = ['$','{','&&','||']
+const options = {
+	urlBlackList: blackList,
+	bodyBlackList: blackList,
+    urlMessage: 'A forbidden expression has been found in the URL',
+    bodyMessage: 'A forbidden expression has been found in the form data'
+}
+app.use(filter(options));
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
@@ -40,7 +51,7 @@ app.use('/admin', adminRouter)
 app.use('/products', productsRouter)
 
 app.use(function (req, res, next) {
-    res.status(404).send("Невалидна страница!")
+    res.status(404).send()
 })
 
 app.listen(process.env.PORT || 3000)
