531ab420c6965fd75ac3d517f3b7f8205a082a24
Nikola <nikislavchev1@abv.bg>
2022-06-22 11:59:38 +0300

    dark mode code improved

--- public/javascripts/theme.js
+++ public/javascripts/theme.js
@@ -1,19 +1,23 @@
 var mode
+var head  = document.getElementsByTagName('head')[0]
+var link
 
 function applyTheme() {
     mode = localStorage.getItem("mode");
-
     if (mode === 'dark') {
-        var head  = document.getElementsByTagName('head')[0]
-        var link  = document.createElement('link')
-
-        link.id    = mode
-        link.rel   = 'stylesheet'
-        link.type  = 'text/css'
-        link.href  = '../../stylesheets/' + mode + '.css'
-        link.media = 'all'
-        
-        head.appendChild(link)
+        if (document.getElementById('dark') == null) {
+            link  = document.createElement('link')
+            link.id    = 'dark'
+            link.rel   = 'stylesheet'
+            link.type  = 'text/css'
+            link.href  = '../../stylesheets/dark.css'
+            link.media = 'all'
+            head.appendChild(link)
+        }
+    } else {
+        if (document.getElementById('dark') != null) {
+            link.parentNode.removeChild(link);
+        }
     }
 }
 
@@ -22,6 +26,10 @@ function runThemeUpdates() {
         document.getElementById("modeSwitch").checked = true
         document.getElementById('fPara').classList.remove("text-muted")
         document.getElementById('fLink').classList.remove("text-muted")
+    } else {
+        document.getElementById("modeSwitch").checked = false
+        document.getElementById('fPara').classList.add("text-muted")
+        document.getElementById('fLink').classList.add("text-muted")
     }
 }
 
@@ -35,87 +43,7 @@ function switchMode() {
     } else {
         mode = 'dark'
     }
-
     localStorage.setItem("mode", mode);
-
-    if (mode === 'dark') {
-        document.body.style.background = "#212121"
-        document.body.style.color = "white"
-
-        if (document.getElementById('imgBg1') != null)
-            document.getElementById('imgBg1').classList.add("img-bg-dynamical")  
-        if (document.getElementById('imgBg2') != null)
-            document.getElementById('imgBg2').classList.add("img-bg-dynamical")
-        if (document.getElementById('imgBg3') != null)
-            document.getElementById('imgBg3').classList.add("img-bg-dynamical")
-
-        document.getElementById('fPara').classList.remove("text-muted")
-        document.getElementById('fLink').classList.remove("text-muted")
-
-        if (document.getElementById('offcanvas') != null) {
-            document.getElementById('offcanvas').style.backgroundColor = "#212121"
-            document.getElementById('offcanvas').style.color = "white"
-        }
-        if (document.getElementsByName('catOption') != null) {
-            var elementArray = document.getElementsByName('catOption')
-            for (i = 0; i < elementArray.length; i++) {
-                elementArray[i].style.backgroundColor = "#181818"
-                elementArray[i].style.color = "white"
-            }
-        }
-
-        if (document.getElementsByClassName('form-control') != null) {
-            var elementArray = document.getElementsByClassName('form-control')
-            for (i = 0; i < elementArray.length; i++) {
-                elementArray[i].style.backgroundColor = "#e7e7e7"
-            }
-        }
-
-        if (document.getElementById('modalContent') != null) {
-            document.getElementById('modalContent').classList.add("modal-content-dynamical")
-        }
-    } else {
-        document.body.style.background = "white"
-        document.body.style.color = "black"
-
-        if (document.getElementById('imgBg1') != null) {
-            document.getElementById('imgBg1').classList.remove("img-bg-dynamical")
-            document.getElementById('imgBg1').classList.remove("img-bg")
-        }
-        if (document.getElementById('imgBg2') != null) {
-            document.getElementById('imgBg2').classList.remove("img-bg-dynamical")
-            document.getElementById('imgBg2').classList.remove("img-bg")
-        }
-        if (document.getElementById('imgBg3') != null) {
-            document.getElementById('imgBg3').classList.remove("img-bg-dynamical")
-            document.getElementById('imgBg3').classList.remove("img-bg")
-        }
-
-        document.getElementById('fPara').classList.add("text-muted")
-        document.getElementById('fLink').classList.add("text-muted")
-
-        if (document.getElementById('offcanvas') != null) {
-            document.getElementById('offcanvas').style.backgroundColor = "white"
-            document.getElementById('offcanvas').style.color = "black"
-        }
-        if (document.getElementsByName('catOption') != null) {
-            var elementArray = document.getElementsByName('catOption')
-            for (i = 0; i < elementArray.length; i++) {
-                elementArray[i].style.backgroundColor = "white"
-                elementArray[i].style.color = "black"
-            }
-        }
-
-        if (document.getElementsByClassName('form-control') != null) {
-            var elementArray = document.getElementsByClassName('form-control')
-            for (i = 0; i < elementArray.length; i++) {
-                elementArray[i].style.backgroundColor = "white"
-            }
-        }
-
-        if (document.getElementById('modalContent') != null) {
-            document.getElementById('modalContent').classList.remove("modal-content-dynamical")
-            document.getElementById('modalContent').style.backgroundColor = "white"
-        }
-    }
+    applyTheme()
+    runThemeUpdates()
 }
